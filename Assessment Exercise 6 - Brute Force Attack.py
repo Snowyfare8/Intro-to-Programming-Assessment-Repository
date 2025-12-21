b67c6c42a4b5eeecdca38e08a1b5cb689ffc9e89
snowyyfare8 <snowytejada08@gmail.com>
2025-12-21 14:14:16 +0300

    Add files via upload

---
+++ Assessment Exercise 6 - Brute Force Attack.py
@@ -0,0 +1,11 @@
+# Assessment Exercise 6
+
+password = "correctpassword"
+password_input = input("Enter password: ")
+
+while password != "correctpassword":
+    print("Incorrect password")
+
+else:
+    password == "correctpassword"
+    print("Correct password")
