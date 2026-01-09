4db8ba608940b9f29746f881b80239b9b5663871
snowyyfare8 <snowytejada08@gmail.com>
2026-01-09 15:32:56 +0300

    Update Assessment Exercise 06 - Brute Force Attack.py

--- Assessment Exercise 06 - Brute Force Attack.py
+++ Assessment Exercise 06 - Brute Force Attack.py
@@ -1,11 +1,19 @@
 # Assessment Exercise 6
 
-password = "correctpassword"
-password_input = input("Enter password: ")
+# 5 password attempts 
+attempts = 5
+# Loop will run until attempts run out or correct password is entered
+while attempts > 0:
+    password_input = input("Enter password: ")
 
-while password != "correctpassword":
-    print("Incorrect password")
+    # Loop will run until correct password is entered
+    if password_input == "1234":
+        print("Correct password")
+        break
+    else: # Incorrect entries deduct an attempt and display it
+        attempts -= 1
+        print("You have " + str(attempts) + " attempts left")
 
-else:
-    password == "correctpassword"
-    print("Correct password")
+# Will run when all password attempts are exhausted
+else: 
+    print("Maximum attempts reached. Law enforcement have been alerted to your location")
