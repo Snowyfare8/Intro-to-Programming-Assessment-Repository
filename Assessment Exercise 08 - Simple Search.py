87e5bc5b51c885aa3590205fa020387705372a47
snowyyfare8 <snowytejada08@gmail.com>
2026-01-09 16:04:22 +0300

    Update Assessment Exercise 08 - Simple Search.py

--- Assessment Exercise 08 - Simple Search.py
+++ Assessment Exercise 08 - Simple Search.py
@@ -1,5 +1,6 @@
 # Assessment Exercise 8
 
+# Define name list
 name_list = {
     "Jake",
     "Zac",
@@ -9,10 +10,15 @@ name_list = {
     "Dave"
 }
 
+# Function to search for name
 def name_search():
-    if "Sam" in name_list:
+    # Function to create user input
+    search_input = input("Input name here: ").capitalize()
+    if search_input in name_list:
         print("name found")
     else:
         print("name not found")
 
+# Calls "name_search" function
 name_search()
+
