53386ec9f08eb2ff2b29907d93dc97acb494aa6e
snowyyfare8 <snowytejada08@gmail.com>
2026-01-09 13:52:13 +0300

    Update Assessment Exercise 02 - Simple Sums.py

--- Assessment Exercise 02 - Simple Sums.py
+++ Assessment Exercise 02 - Simple Sums.py
@@ -1,7 +1,11 @@
 # Assessment Exercise 2
 
+# Assign integer value to variables "a" and "b"
 a = 8
 b = 10
+
+# Assign mathematical operation to variable "c"
 c = a + b
 
+# Display mathematical operation "c"
 print(c)
