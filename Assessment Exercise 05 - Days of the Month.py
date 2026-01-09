7ca33f3c0a289d5eda81f05c2453bc7e30c06130
snowyyfare8 <snowytejada08@gmail.com>
2026-01-09 16:08:49 +0300

    Update Assessment Exercise 05 - Days of the Month.py

--- Assessment Exercise 05 - Days of the Month.py
+++ Assessment Exercise 05 - Days of the Month.py
@@ -1,7 +1,11 @@
 # Assessment 5
 
-months = {
+# Asks user for month from 1-12 (Jan-Dec)
+def month_selection():
 
+# Dictionary of months and their respective days
+
+    months = {
     "1":31, 
     "2":28, 
     "3":31, 
@@ -14,12 +18,24 @@ months = {
     "10":30, 
     "11":30, 
     "12":31
+    }
 
-}
-
-def month_selection():
-    user_input = (str(input("Enter month by number here: ")))
-    days = months.get(user_input)
-    print(days)
+    user_input = str(input("Enter month by number here: "))
+    # Activates if user inputs 2 (Feb)
+    if user_input == "2":
+        # Asks if current year is a leap year
+        leap_year_question = str(input("Leap year? ")).lower()
+        if leap_year_question == "yes": # Adds an additional day to Feb if yes
+            months["2"] = 29
+            days = months.get(user_input)
+            print(days)
+        else: # No additional day to Feb if not
+            leap_year_question == "no"
+            days = months.get(user_input)
+            print(days)
+    else:
+        user_input != "2"
+        days = months.get(user_input)
+        print(days)
 
 month_selection()
