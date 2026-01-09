beb2c301e2f6cc9f57c1e3118626c1d64d0aa172
snowyyfare8 <snowytejada08@gmail.com>
2026-01-09 15:00:51 +0300

    Update Assessment Exercise 04 - Primitive Quiz.py

--- Assessment Exercise 04 - Primitive Quiz.py
+++ Assessment Exercise 04 - Primitive Quiz.py
@@ -1,14 +1,17 @@
 # Assessment Exercise 4
 
+# Create class "french_city"
 class french_city:
     def __init__(self, name):
         self.name = name
 
+# Define four variables cities to be used later
 fc1 = french_city("Lyon")
 fc2 = french_city("Marseille")
 fc3 = french_city("Paris")
 fc4 = french_city("Orleans")
 
+# Create question and answer function
 def fra_capital_question ():
     print("What is the capital of France?")
     print("A. " + fc1.name)
@@ -16,7 +19,8 @@ def fra_capital_question ():
     print("C. " + fc3.name)
     print("D. " + fc4.name)
 
-    answer = input("Enter your answer here: ")
+    # Capitalizes users answers, such that the system recognizes the user's answer as being capitalized regardless of initial input
+    answer = input("Enter your answer here: ").capitalize() 
     if answer ==  fc1.name:
         print("Wrong answer")
     elif answer == fc2.name:
@@ -26,4 +30,5 @@ def fra_capital_question ():
     else:
         print("Wrong answer")
 
+# Call function
 fra_capital_question()
