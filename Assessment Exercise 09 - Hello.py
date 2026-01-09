0e7703c46f2c4ce1de7cecce194a3c7baad48a9b
snowyyfare8 <snowytejada08@gmail.com>
2026-01-09 15:37:56 +0300

    Enhance comments in Hello.py for clarity
    Added comments to clarify function purposes.

--- Assessment Exercise 09 - Hello.py
+++ Assessment Exercise 09 - Hello.py
@@ -1,10 +1,14 @@
 # Exercise Assessment 9
 
+# Function that prints hello
 def hello():
     print("hello")
 
+# Function that calls "hello"
 def main():
     hello()
 
+# Will call function "main" if __name__ == __main__
 if __name__ == "__main__":
+
     main()
