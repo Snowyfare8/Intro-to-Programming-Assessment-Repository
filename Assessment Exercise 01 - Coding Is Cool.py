6502da307eba5de77e3d56fe7dd944b1224554d8
snowyyfare8 <snowytejada08@gmail.com>
2026-01-09 13:50:33 +0300

    Enhance comments for clarity in coding exercise
    Added comments to clarify variable assignments and print statement.

--- Assessment Exercise 01 - Coding Is Cool.py
+++ Assessment Exercise 01 - Coding Is Cool.py
@@ -1,7 +1,9 @@
 # Assessment Exercise 1
 
+# Assign value to variables
 word1 = "Coding"
 word2 = "Is"
 word3 = "Cool"
 
+# print variables to say "Coding Is Cool
 print(word1 + " " + word2 + " " + word3)
